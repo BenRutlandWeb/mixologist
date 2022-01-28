7ee0311c7b8b4814b4d16449564ab5e13a5e68a6
Ben Rutland <ben@10degrees.uk>
2022-01-28 16:55:50 +0000

    ingredient checkbox component

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,32 @@
 <template>
-    <div>
-        <h1 class="font-mono">Hello World!</h1>
+    <h1 class="text-2xl">Mixologist</h1>
 
-        <div class="p-4 grid gap-8">
-            <ul class="flex flex-wrap gap-1">
-                <li v-for="ingredient in allIngredients" :key="ingredient.name">
-                    <label>
-                        <input
-                            type="checkbox"
-                            v-model="barIngredients"
-                            :value="ingredient.name"
-                            class="sr-only peer"
-                        />
-                        <div
-                            class="select-none cursor-pointer border rounded p-2 flex gap-2 items-center peer-checked:border-red-500"
-                        >
-                            {{ ingredient.name }}
-                        </div>
-                    </label>
-                </li>
-            </ul>
-            <ul class="grid gap-2">
-                <li
-                    class="border rounded p-2"
-                    v-for="cocktail in cocktails()"
-                    :key="cocktail.name"
-                >
-                    <h2>{{ cocktail.name }}</h2>
-                    <p class="text-gray-500">
-                        {{
-                            [
-                                ...(cocktail.ingredients ?? []),
-                                ...(cocktail.optionalIngredients ?? []),
-                            ].join(", ")
-                        }}
-                    </p>
-                </li>
-            </ul>
-        </div>
+    <div class="p-4 grid gap-8">
+        <ul class="flex flex-wrap gap-1">
+            <li v-for="ingredient in allIngredients" :key="ingredient.name">
+                <ingredient-checkbox
+                    v-model="barIngredients"
+                    :value="ingredient.name"
+                />
+            </li>
+        </ul>
+        <ul class="grid gap-2">
+            <li
+                class="border rounded p-2"
+                v-for="cocktail in cocktails()"
+                :key="cocktail.name"
+            >
+                <h2>{{ cocktail.name }}</h2>
+                <p class="text-gray-500">
+                    {{
+                        [
+                            ...(cocktail.ingredients ?? []),
+                            ...(cocktail.optionalIngredients ?? []),
+                        ].join(", ")
+                    }}
+                </p>
+            </li>
+        </ul>
     </div>
 </template>
 
