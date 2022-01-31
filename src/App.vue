6604ef481b9c5996bf78792b5cead55cf0680f81
Ben Rutland <benrutland@hotmail.com>
2022-01-31 21:04:06 +0000

    changed to yml db

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,50 @@
 <template>
-    <h1 class="text-2xl">Mixologist</h1>
+  <h1 class="text-2xl">Mixologist</h1>
 
-    <div class="p-4 grid gap-8">
-        <ul class="flex flex-wrap gap-1">
-            <li v-for="ingredient in allIngredients" :key="ingredient.name">
-                <ingredient-checkbox
-                    v-model="barIngredients"
-                    :value="ingredient.name"
-                />
-            </li>
-        </ul>
-        <ul class="grid gap-2">
-            <li
-                class="border rounded p-2"
-                v-for="cocktail in cocktails()"
-                :key="cocktail.name"
-            >
-                <h2>{{ cocktail.name }}</h2>
-                <p class="text-gray-500">
-                    {{
-                        [
-                            ...(cocktail.ingredients ?? []),
-                            ...(cocktail.optionalIngredients ?? []),
-                        ].join(", ")
-                    }}
-                </p>
-            </li>
-        </ul>
-    </div>
+  <div class="p-4 grid gap-8">
+    <ul class="flex flex-wrap gap-1">
+      <li v-for="ingredient in allIngredients" :key="ingredient.name">
+        <ingredient-checkbox
+          v-model="barIngredients"
+          :value="ingredient.name"
+        />
+      </li>
+    </ul>
+    <ul class="grid gap-2">
+      <li
+        class="border rounded p-2"
+        v-for="cocktail in cocktails()"
+        :key="cocktail.name"
+      >
+        <h2>{{ cocktail.name }}</h2>
+        <p class="text-gray-500">
+          {{
+            [
+              ...(cocktail.ingredients ?? []),
+              ...(cocktail.optionalIngredients ?? []),
+            ].join(", ")
+          }}
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
 import { ref } from "@vue/reactivity";
 import { watchEffect } from "@vue/runtime-core";
-import COCKTAILS from "./db/cocktails.json";
-import INGREDIENTS from "./db/ingredients.json";
+import COCKTAILS from "./db/cocktails.yml";
+import INGREDIENTS from "./db/ingredients.yml";
 
 const BAR_INGREDIENTS = JSON.parse(
-    localStorage.getItem("bar-ingredients") ?? "[]"
+  localStorage.getItem("bar-ingredients") ?? "[]"
 );
 
 // filter the cocktails to only thse that can be made
 function filterCocktails(cocktails, ingredients) {
-    return cocktails.filter((cocktail) => {
-        return cocktail.ingredients.every((i) => ingredients.includes(i));
-    });
+  return cocktails.filter((cocktail) => {
+    return cocktail.ingredients.every((i) => ingredients.includes(i));
+  });
 }
 
 const allIngredients = INGREDIENTS;
@@ -52,14 +52,11 @@ const allIngredients = INGREDIENTS;
 const barIngredients = ref(BAR_INGREDIENTS);
 
 function cocktails() {
-    return filterCocktails(COCKTAILS, barIngredients.value);
+  return filterCocktails(COCKTAILS, barIngredients.value);
 }
 
 function updateLocalStorage() {
-    localStorage.setItem(
-        "bar-ingredients",
-        JSON.stringify(barIngredients.value)
-    );
+  localStorage.setItem("bar-ingredients", JSON.stringify(barIngredients.value));
 }
 
 watchEffect(updateLocalStorage);
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import components from "unplugin-vue-components/vite";
+import content from "@originjs/vite-plugin-content";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        components({
-            /* options */
-        }),
-    ],
+  plugins: [
+    vue(),
+    components({
+      /* options */
+    }),
+    content(),
+  ],
 });
